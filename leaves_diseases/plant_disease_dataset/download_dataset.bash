d0711b5bed1598a31bbe679486415d6604d5e5cf
anodicpassion <pratikpawarkarad@gmail.com>
2025-05-14 17:34:36 +0530

    updated download path to current path

--- leaves_diseases/plant_disease_dataset/download_dataset.bash
+++ leaves_diseases/plant_disease_dataset/download_dataset.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -L -o ~/Downloads/new-plant-diseases-dataset.zip\
+curl -L -o new-plant-diseases-dataset.zip
   https://www.kaggle.com/api/v1/datasets/download/vipoooool/new-plant-diseases-dataset
